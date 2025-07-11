628d56cd101c1de220d62530a17169fc9f190308
Rubiksel <ejderaksel@gmail.com>
2025-07-11 11:43:14 -0500

    refactor: remove guard and update truncation pipe length

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
-import { Routes, withComponentInputBinding } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { PokemonDetailedViewComponent } from './pokemon-detailed-view/pokemon-detailed-view.component';
-import { listPasswordGuard } from './list-password.guard';
+import { Routes, withComponentInputBinding } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
+import { PokemonDetailedViewComponent } from "./pokemon-detailed-view/pokemon-detailed-view.component";
+import { listPasswordGuard } from "./list-password.guard";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: "", component: HomeComponent },
   {
-    path: 'pokemons',
+    path: "pokemons",
     component: PokemonListComponent,
-    canActivate: [listPasswordGuard],
+    // canActivate: [listPasswordGuard],
   },
   {
-    path: 'pokemons/:name',
+    path: "pokemons/:name",
     component: PokemonDetailedViewComponent,
   },
 ];
